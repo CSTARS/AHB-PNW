a225e61d1fa723370784a1025b20bc3e8fd16934
Olga <prilepova@gmail.com>
2013-07-16 03:11:33 +0300

    added the core code that must be loaded with the spreadsheet to the UserINterface.js

--- models/3pg/js/UserInterface.js
+++ models/3pg/js/UserInterface.js
@@ -1,16 +1,25 @@
 
 // this is required to be copied inside spreadsheet
 // ie.. it cannot be side loaded
+
+// this is required to be copied inside spreadsheet
+// ie.. it cannot be side loaded
 function onOpen() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var menuEntries;
   menuEntries = [
-                  {name: "Run Tests", functionName: "runModel31Test"},
+                  {name: "Run tests from sheet TestSetup", functionName: "runModelXTest"},
+                  {name: "2 years like GreenWood", functionName: "runModel31"},
                   {name: "Model 4 Months", functionName: "runModel4"},
                   {name: "Model 2 Years", functionName: "runModel24"},
                   {name: "Model 5 Years", functionName: "runModel60"},
                   {name: "Model 10 Years", functionName: "runModel120"}];
   ss.addMenu("Action", menuEntries);
+}
+
+
+function runModel31(){
+   m3PG.run(31);  
 }
 
 function runModel4(){
@@ -21,23 +30,24 @@ function runModel24(){
    m3PG.run(24);
 }
 
-function runModel31Test(){
-  var lengthOfGrowth = 31;//growth for 31 month (2 years planted in march, cut in september assumption)
-  var allTestValues = readTestInputs();
 
+function runModelXTest(){
+  // assume planted in march, cut in september
+  //var allTestValues = readTestInputs("Form Responses 1");
+  var allTestValues = m3PGIO.readTestInputs("TestSetup");
+  var locationAsString = allTestValues["Location"][0] + "";
+      
   //loop over var names, values, output var names
   
   //column names are assumed, and the lack of whitespace between comma-separated values.
   var inputVarNameArray = allTestValues["Input Variable"];
   var inputVarValsArray = allTestValues["Input Values"];
-  var outputVarNamesArray = allTestValues["Output Variables"];
+  var outputVarName = allTestValues["Output Variable"];
   
   //////////Set up the original version of the model:////////////
 
   //work with offsets and weather data later
   
-  var isCoppiced = false;
-  var willCoppice = false;
   var yearToCoppice;
   var coppiceInterval;
   var monthToCoppice;
@@ -48,112 +58,100 @@ function runModel31Test(){
   
   //calculate fNutr and add here;
   g.fNutr=m3PGFunc.fNutr(g.fN0, g.FR);
-  
-  var weatherMap = {};
-  var s = {}; //soilMap
-  var dateMap = {};
-  m3PGIO.readWeather(weatherMap, s, dateMap);
-  
-  var currentDate = dateMap["datePlanted"];
-  
-  var plantedMonth = currentDate.getMonth();
-  var currentMonth = currentDate.getMonth();
-  
-  if (dateMap["dateCoppiced"] != undefined){
-    yearToCoppice = dateMap["dateCoppiced"].getYear();
-    monthToCoppice = dateMap["dateCoppiced"].getMonth();
-    coppiceInterval = dateMap["yearsPerCoppice"];
-    willCoppice = true;
-  }
-  
-  log("Month of Planting = " + currentMonth);
-  
-  var step = 0;
-  
-  var d = weatherMap[currentMonth];
-  
-  log(d);
-  
-  var keysInOrder = ["Date", "VPD", "fVPD", "fT", "fFrost", "PAR", "xPP", "Intcptn","ASW","CumIrrig","Irrig","StandAge","LAI","CanCond","Transp","fSW","fAge","PhysMod","pR","pS","litterfall","NPP","WF","WR","WS", "W"];    
-
-  
-  
+ 
   ////////////////loop over different scenarios////////////////
-  
   for (var testNum = 0; testNum<inputVarNameArray.length; testNum++){
     var inputVarName = inputVarNameArray[testNum];
-    var inputVarValsAsText = inputVarValsArray[testNum];
+    var inputVarValsAsText = inputVarValsArray[testNum] + ""; //important hack to make sure this value is a string even when it's a single number
     inputVarValsAsText.replace(/\s/g, "");
     var inputVarVals = inputVarValsAsText.split(',');
-    Logger.log(inputVarVals);
-    var outputVarNamesAsText = outputVarNamesArray[testNum];
-    Logger.log(outputVarNamesAsText);
-    outputVarNamesAsText.split(" ").join(""); //FOR some reason nothing works in removing white spaces?? FIXME: make this work later
-    var outputVarNames = outputVarNamesAsText.split(',');
-    Logger.log(outputVarNames);
-    for (var ind = 0; ind<outputVarNames.length; ind++){
-      Logger.log("||" + outputVarNames[ind] + "||");
-    }
+    
+    var outputVarNameAsText = outputVarName[testNum];
+    outputVarNameAsText.split(" ").join(""); //FOR some reason nothing works in removing white spaces?? FIXME: make this work later
+    var outputVarName = outputVarNameAsText;
+    
+    var weatherMap = {};
+    var s = {}; //soilMap
+    var dateMap = {};
+    m3PGIO.readWeatherFromRequest(weatherMap, s, dateMap, locationAsString.split(","));
+    //add dates of the experiment setup
+    dateMap["datePlanted"] = allTestValues["Planted Date"][testNum];
+    dateMap["dateCoppiced"] = allTestValues["Coppice Date"][testNum];
+    dateMap["yearsPerCoppice"] = allTestValues["Coppice Interval Years"][testNum];
+    var dateEnd = new Date(allTestValues["Grow Until Date"][testNum]);
+    var dateStart = new Date(dateMap["datePlanted"]);
+    //Logger.log(allTestValues["Grow Until Date"][testNum]);
+    //Logger.log("dateStart="+dateStart+"; dateEnd=" + dateEnd+"; passed=" + ((dateEnd.getYear()-dateStart.getYear())*12 + dateEnd.getMonth()-dateStart.getMonth()));
+    
+    var lengthOfGrowth = ((dateEnd.getYear()-dateStart.getYear())*12 + dateEnd.getMonth()-dateStart.getMonth()); //in months
+    var currentDate = new Date(dateMap["datePlanted"]);
     
     //TODO: deal with output variables - AKA get only them and then print them
-       
+    
     
     var sheetName = "Test Outputs: " + inputVarName;
-    runFirstTime31Test(sheetName,lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap);
+    runFirstTimeXTest(sheetName,lengthOfGrowth,currentDate);
     for (var k = 0; k<inputVarVals.length; k++){
-      runSubsequentTimes31Test(inputVarName,inputVarVals[k], k, sheetName,lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap);
+      var willCoppice = false;
+      var isCoppiced = false;
+      currentDate = new Date(dateMap["datePlanted"]);
+      
+      var plantedMonth = currentDate.getMonth();
+      var currentMonth = currentDate.getMonth();
+      
+      if (dateMap["dateCoppiced"] != undefined){
+        yearToCoppice = dateMap["dateCoppiced"].getYear();
+        monthToCoppice = dateMap["dateCoppiced"].getMonth();
+        coppiceInterval = dateMap["yearsPerCoppice"];
+        willCoppice = true;
+      }
+      
+      //log("Month of Planting = " + currentMonth);
+      
+      var step = 0;
+      
+      var d = weatherMap[currentMonth];
+      
+      //log(d);
+      
+      var keysInOrder = ["Date", "VPD", "fVPD", "fT", "fFrost", "PAR", "xPP", "Intcptn","ASW","CumIrrig","Irrig","StandAge","LAI","CanCond","Transp","fSW","fAge","PhysMod","pR","pS","litterfall","NPP","WF","WR","WS", "W"];    
+      
+      runSubsequentTimesXTest(inputVarName,inputVarVals[k], k, sheetName,lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap,outputVarName);
     }
   }
-  /*
-  var inputVarName = "StockingDensity";
-  var inputVarVals = ["1000", "2000", "3000"];
-  
-  runFirstTime31Test(inputVarName);
-  for (var k = 0; k<inputVarVals.length; k++){
-    runSubsequentTimes31Test(inputVarName,inputVarVals[k], k);
-  }*/
+ 
 }  
 
-function runFirstTime31Test(sheetName,lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap){
-    //assume cutting in september
-  var resultRows =  m3PG.runCurrentSetup(lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap);
-        
-      //problem: runs model one extra time
-  
+function runFirstTimeXTest(sheetName,lengthOfGrowth,currentDate,d){
+    
   //init main data structure
   var rows = [];
-  var dateIndex = resultRows[0].indexOf("Date");
-  for (var i = 0; i < resultRows.length; i++) {       
-      Logger.log("resultRows[i][dateIndex] = " + resultRows[i][dateIndex]);
-      var newRow = [];
-      newRow.push(resultRows[i][dateIndex]);
+  var newRow = [];
+  newRow.push("Date");
+  rows.push(newRow);
+  for (var i = 0; i < lengthOfGrowth; i++) {     
+      newRow = [];
+      newRow.push((currentDate.getMonth()+1) + "/" + currentDate.getYear());
       rows.push(newRow);
+      currentDate.setMonth(currentDate.getMonth() + 1);      
     }
 
-  writeRowsToNewSheet(rows, sheetName);
+  m3PGIO.writeRowsToNewSheet(rows, sheetName);
 }
 
 
-function runSubsequentTimes31Test(inputVarName, inputVarValue, indexToPrint, sheetName,lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap){
-
-   //NOTE: only constant variables are modifiable for now (AKA globals "g")
+function runSubsequentTimesXTest(inputVarName, inputVarValue, indexToPrint, sheetName,lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap,outputVariable){
+  //NOTE: only constant variables are modifiable for now (AKA globals "g")
   g[inputVarName] = inputVarValue;
   
   //big loop here
-  
-   //only one variable supported at the moment - need to change writing to rows for more
-  var outputVariable = "NPP"
   var header = outputVariable + "," + inputVarName + "=" + inputVarValue; //TODO: deal with upper/lower cases?
 
-  //TODO: take out computation part (wher you replace x values)
-  
-  var resultRows = m3PG.runCurrentSetup(lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap);
-  
+  //TODO: take out computation part (wher you replace x values)  
+  var reprintHeaders = false;
+  var resultRows = m3PG.runCurrentSetup(lengthOfGrowth,g,d,s,keysInOrder,step,plantedMonth,currentDate,currentMonth,yearToCoppice,monthToCoppice,coppiceInterval,willCoppice,isCoppiced,weatherMap,reprintHeaders);  
   var index = resultRows[0].indexOf(outputVariable);
-  
-  
-  //TODO write in above function
-  
+
     var rows = [];
   // key => var inputVar = inputVars[k];
   // index = k = column to print into
@@ -164,7 +162,7 @@ function runSubsequentTimes31Test(inputVarName, inputVarValue, indexToPrint, she
   rows.push(currentRow); //the header is set,
   //now loop over actual results
   for (var i = 1; i < resultRows.length; i++) {       
-    Logger.log("resultRows[i][index] = " + resultRows[i][index]);
+   // Logger.log("resultRows[i][index] = " + resultRows[i][index]);
     currentRow = [];
     currentRow.push(resultRows[i][index]);
     rows.push(currentRow);
@@ -172,61 +170,8 @@ function runSubsequentTimes31Test(inputVarName, inputVarValue, indexToPrint, she
   
   //PROBLEM HERE (make column offset?)
   //big loop here
-  writeRowsToSheetWithOffset(rows, sheetName, indexToPrint+2);
+  m3PGIO.writeRowsToSheetWithOffset(rows, sheetName, indexToPrint+2);
 }
-
-function writeRowsToNewSheet(array, newSheetName){
-  //TODO: place into IO code
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var existingSheet = spreadsheet.getSheetByName(newSheetName);
-  var resultSheet;
-  if (existingSheet != undefined || existingSheet!=""){
-    resultSheet = existingSheet;
-  }
-  else {
-    resultSheet = spreadsheet.insertSheet(newSheetName);        
-  }
-  var range = resultSheet.getRange(1, 1,
-                                   array.length, array[0].length);
-  range.setValues(array);
-}
-
-function writeRowsToSheetWithOffset(array, sheet, offsetFromTheLeft){
-  //TODO: place into IO code
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var resultSheet = spreadsheet.getSheetByName(sheet); //TODO: decide on where this can be taken out into. Output         
-  var range = resultSheet.getRange(1, offsetFromTheLeft,
-                                   array.length, array[0].length);
-  range.setValues(array);
-}
-
-
-//TODO: place this in IO code
-
-function readTestInputs(){
-  var keyValMap = {};
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet(); 
-  //var columns = spreadsheet.getLastColumn();
-  var sheet = spreadsheet.getSheetByName("Form Responses 1"); 
-  var data = sheet.getDataRange().getValues();
-  var keys = data[0];  
-  
-  for (var keyNum = 0; keyNum < keys.length; keyNum++){
-    keyValMap[keys[keyNum]] = [];
-  }
-  
-  for (var rowNumber = 1; rowNumber < data.length; rowNumber++) { //start from second row, value is in the forth column, index 3
-    Logger.log(data[rowNumber]);
-    var rowData = data[rowNumber];
-    for (var keyNum = 0; keyNum < keys.length; keyNum++){
-      keyValMap[keys[keyNum]].push(rowData[keyNum]);
-    }
-  } 
-  
-  Logger.log(keyValMap);
-  
-  return keyValMap;
-} 
 
 
 function runModel60(){
